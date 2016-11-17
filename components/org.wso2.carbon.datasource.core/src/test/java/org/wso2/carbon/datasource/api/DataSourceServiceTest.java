0697e49e877dcff096083ba04613c2514cb1f605
Niranjan-K <niranjan.karu@gmail.com>
2016-11-17 19:20:50 +0530

    19: Unit test for createDataSource API

--- components/org.wso2.carbon.datasource.core/src/test/java/org/wso2/carbon/datasource/api/DataSourceServiceTest.java
+++ components/org.wso2.carbon.datasource.core/src/test/java/org/wso2/carbon/datasource/api/DataSourceServiceTest.java
@@ -18,12 +18,23 @@ package org.wso2.carbon.datasource.api;
 import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+import org.w3c.dom.Document;
 import org.wso2.carbon.datasource.core.BaseTest;
 import org.wso2.carbon.datasource.core.api.DataSourceService;
+import org.wso2.carbon.datasource.core.beans.DataSourceDefinition;
 import org.wso2.carbon.datasource.core.exception.DataSourceException;
 import org.wso2.carbon.datasource.core.impl.DataSourceServiceImpl;
+import org.xml.sax.SAXException;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class DataSourceServiceTest extends BaseTest {
 
@@ -39,5 +50,30 @@ public class DataSourceServiceTest extends BaseTest {
     public void getDataSourceTest() throws DataSourceException {
         Object dataSourceObject = dataSourceService.getDataSource("WSO2_CARBON_DB_2");
         Assert.assertNotNull(dataSourceObject, "test datasource \"WSO2_CARBON_DB_2\" should not be null");
+    }
+
+    @Test
+    public void createDataSourceTest() throws DataSourceException {
+        try {
+            String dsXMLConfiguration = "<definition type=\"RDBMS\"><configuration><url>jdbc:h2:" +
+                    "./database/WSO2_CARBON_DB;DB_CLOSE_ON_EXIT=FALSE;" +
+                    "LOCK_TIMEOUT=60000</url><username>wso2carbon</username><password>wso2carbon</password" +
+                    "><driverClassName>org.h2" +
+                    ".Driver</driverClassName><maxActive>50</maxActive><maxWait>60000</maxWait><testOnBorrow>true" +
+                    "</testOnBorrow><validationQuery>SELECT " +
+                    "1</validationQuery><validationInterval>30000</validationInterval><defaultAutoCommit>false" +
+                    "</defaultAutoCommit></configuration></definition>";
+            DataSourceDefinition dataSourceDefinition = new DataSourceDefinition();
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new ByteArrayInputStream(dsXMLConfiguration.getBytes(StandardCharsets.UTF_8)));
+            JAXBContext ctx = JAXBContext.newInstance(dataSourceDefinition.getClass());
+            dataSourceDefinition = (DataSourceDefinition) ctx.createUnmarshaller().unmarshal(doc);
+
+            Object dataSourceObj = dataSourceService.createDataSource(dataSourceDefinition);
+            Assert.assertNotNull(dataSourceObj, "Failed to create datasource Object!");
+        } catch (DataSourceException | ParserConfigurationException | SAXException | IOException | JAXBException e) {
+            Assert.fail("Threw an exception while creating datasource Object");
+        }
     }
 }
