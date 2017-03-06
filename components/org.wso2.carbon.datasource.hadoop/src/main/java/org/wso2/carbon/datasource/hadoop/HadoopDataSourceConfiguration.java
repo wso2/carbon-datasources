58e4921064ea70c829388118503ad403ea9abc22
gokul <royalgok@gmail.com>
2017-03-06 11:33:04 +0530

    Update copyright in Hadoop data source

--- components/org.wso2.carbon.datasource.hadoop/src/main/java/org/wso2/carbon/datasource/hadoop/HadoopDataSourceConfiguration.java
+++ components/org.wso2.carbon.datasource.hadoop/src/main/java/org/wso2/carbon/datasource/hadoop/HadoopDataSourceConfiguration.java
@@ -1,5 +1,5 @@
 /*
-*  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 *
 *  WSO2 Inc. licenses this file to you under the Apache License,
 *  Version 2.0 (the "License"); you may not use this file except
@@ -31,7 +31,7 @@ public class HadoopDataSourceConfiguration {
 
     @XmlElement(name = "property")
     public List<HadoopDataSourceProperty> getProperties() {
-        return (properties);
+        return properties;
     }
 
     public void setProperties(List<HadoopDataSourceProperty> properties) {
